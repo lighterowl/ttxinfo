d1bb41d5430cea9e2fdc24204e178126c6cd881b
Daniel Kamil Kozar <dkk089@gmail.com>
2014-06-24 20:47:40 +0200

    read a TS and parse the TS header

--- ttxinfo.c
+++ ttxinfo.c
@@ -9,7 +9,57 @@
 #define TS_PACKET_SIZE 188
 #endif
 
+#define TS_SYNC_BYTE 0x47
 
+/* parse one packet pointed to by 'tspkt', and return a pointer to the first
+ * data byte inside the packet. if the TS packet is in any way invalid, or its
+ * PID does not match 'pid', return NULL. 'pusi' is set to the value of the
+ * payload unit start indicator in the header. */
+static const uint8_t *parse_ts_packet(const uint8_t *tspkt, uint16_t pid,
+    unsigned int *pusi)
+{
+    uint16_t this_pid;
+    if(tspkt[0] != TS_SYNC_BYTE) return NULL;
+    if((tspkt[1] & 0x80)) return NULL; /* transport_error_indicator */
+    *pusi = (tspkt[1] & 0x40); /* payload_unit_start_indicator */
+    
+    this_pid = (((tspkt[1] & 0x1f) << 8) | tspkt[2]);
+    if(this_pid != pid) return NULL;
+    
+    if((tspkt[3] & 0xc0)) return NULL; /* transport_scrambling_indicator */
+    
+    /* teletext packets must have adaptation_field_control equal to 01
+     * (only payload) or 10 (only adaptation). the adaptation field is of no
+     * interest to us. */
+    if(((tspkt[3] & 0x30) >> 4) != 1) return NULL;
+    
+    /* ignore the continuity counter. */
+    return tspkt + 4;
+}
+
+#define TS_BUFSIZE_NUM_PACKETS 20
+
+static void read_ts(FILE *tsfile, uint16_t pid)
+{
+    /* try to read TS_BUFSIZE_NUM_PACKETS packets at once. */
+    uint8_t ts_pkts[TS_PACKET_SIZE * TS_BUFSIZE_NUM_PACKETS];
+    size_t pkts_read;
+    while((pkts_read = fread(ts_pkts, TS_PACKET_SIZE,
+        TS_BUFSIZE_NUM_PACKETS, tsfile)) > 0)
+    {
+        for(size_t i = 0 ; i < pkts_read ; ++i)
+        {
+            unsigned int pusi;
+            const uint8_t *pes_pkt = parse_ts_packet(
+                ts_pkts + (i * TS_PACKET_SIZE), pid, &pusi);
+            if(pes_pkt)
+            {
+            }
+        }
+    }
+}
+
+#undef TS_BUFSIZE_NUM_PACKETS
 
 /* the largest valid PID value. */
 #define PID_MAX ((2 << 13) - 1)
@@ -65,8 +115,9 @@ int main(int argc, char **argv)
     int rv = parse_args(argc, argv, &pid, &tsfile);
     if(rv != 0)
     {
-        return parse_rv;
+        return rv;
     }
+    read_ts(tsfile, pid);
     
     fclose(tsfile);
     return 0;
